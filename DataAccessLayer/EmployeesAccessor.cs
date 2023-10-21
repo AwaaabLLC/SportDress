7dc9d9e255fa58c5dd85c77cc54404f3fb1f3040
Awaab Elamin <AwaabElamin@gmail.com>
2023-10-21 17:54:34 -0500

    add internal dbconnection class

--- DataAccessLayer/EmployeesAccessor.cs
+++ DataAccessLayer/EmployeesAccessor.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using IDataAccessLayer;
+using System.Data.SqlClient;
 namespace DataAccessLayer
 {
-    public class EmployeesAccessor
+    public class EmployeesAccessor : IEmployeeAccessor
     {
         public bool verifyEmployee(string username, string password)
         {
             bool result = false;
+            SqlConnection conn = DBConnection.getConnection();
+            var cmd = new SqlCommand("");
             return result;
         }
     }
