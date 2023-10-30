da287e8b9104088151b2e8ee80c164fea3a41939
Awaab Elamin <AwaabElamin@gmail.com>
2023-10-30 13:01:05 -0500

    upload last update

--- PresentationLayer2/obj/Debug/net6.0-windows/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer2/obj/Debug/net6.0-windows/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2983933c3c36c2241721fedf44875772fac92f2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
